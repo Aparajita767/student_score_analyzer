1bd7bc6afebe825bc42e860fd42f164869a5f418
Aparajita767 <aparajita767@gmail.com>
2025-06-30 14:11:50 +0530

    chore: added logging configuration

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import routing, FastAPI
 from app.routes import upload
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format= '%(asctime)%s [%(levelname)%s] %(name)%s : %(message)s'
+)
 
 app = FastAPI()
 
 @app.get('/')
 def read_root():
+    logging.info('Root endpoint hit')
     return {'message':'Welcome to Student Score Analyzer Dashboard'}
 
 app.include_router(upload.upload_router)
